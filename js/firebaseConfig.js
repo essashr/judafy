f76d0bf86ea045e8807a52d2d6e24138a0e84377
Thomaz Silva <thomazaugustoth13@gmail.com>
2024-06-29 17:44:41 -0300

    atualizando env variaveis

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -4,7 +4,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: "process.env.DATABASE_URL",
+    databaseURL: process.env.DATABASE_URL,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
